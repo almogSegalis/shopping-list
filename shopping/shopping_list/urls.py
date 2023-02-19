82ae409af262c3bbc8a62b894243722f201866ab
Almog Segalis <almogsegalis@gmail.com>
2023-02-19 22:14:49 +0200

    added add item page

--- shopping/shopping_list/urls.py
+++ shopping/shopping_list/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'shopping_list'
+
 urlpatterns = [
-    path('', views.main_shopping_list),
+    path('', views.main_shopping_list, name="list"),
+    path('add/', views.add_item, name="add"),
 ]
