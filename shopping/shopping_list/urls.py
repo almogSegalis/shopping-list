f203b6e48f756dffb574edb98880df6c2aaea1e4
Almog Segalis <almogsegalis@gmail.com>
2023-02-20 17:49:44 +0200

    add delete button

--- shopping/shopping_list/urls.py
+++ shopping/shopping_list/urls.py
@@ -7,4 +7,5 @@ app_name = 'shopping_list'
 urlpatterns = [
     path('', views.main_shopping_list, name="list"),
     path('add/', views.add_item, name="add"),
+    path('delete_item/<item_id>', views.delete_item, name="delete_item")
 ]
