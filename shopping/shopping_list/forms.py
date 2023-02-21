336248a489b770dea465be8bc59e7d26c1613771
Almog Segalis <almogsegalis@gmail.com>
2023-02-21 11:11:47 +0200

    pertty ui

--- shopping/shopping_list/forms.py
+++ shopping/shopping_list/forms.py
@@ -5,3 +5,13 @@ class AddItem(forms.ModelForm):
 	class Meta:
 		model = models.Item
 		fields = ['item']
+		labels = {
+			'item': '',
+		}
+		widgets = {
+			'item': forms.TextInput(attrs = {
+				'class': 'form-control', 
+				'placeholder':'Item name', 
+				'style':'width:75%'
+				})
+		}
