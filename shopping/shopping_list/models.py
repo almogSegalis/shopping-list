e07364f0d87fbfeb1df76b60a297c630c70e4417
Almog Segalis <almogsegalis@gmail.com>
2023-02-22 17:59:47 +0200

    add tags with colors

--- shopping/shopping_list/models.py
+++ shopping/shopping_list/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
-    # add author
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50) # bootstrap color
+    
     def __str__(self):
         return self.name
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
