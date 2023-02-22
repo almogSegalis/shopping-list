22262328005c9bc79c412a4818c1077b4a9f2c84
Almog Segalis <almogsegalis@gmail.com>
2023-02-22 21:36:06 +0200

    add tags filter

--- shopping/shopping_list/urls.py
+++ shopping/shopping_list/urls.py
@@ -7,5 +7,6 @@ app_name = 'shopping_list'
 urlpatterns = [
     path('', views.main_shopping_list, name="list"),
     path('add_item', views.add_item, name="add_item"),
-    path('delete_item/<item_id>', views.delete_item, name="delete_item")
+    path('delete_item/<item_id>', views.delete_item, name="delete_item"),
+    path('item_list/<tag_name>', views.main_shopping_list ,name="item_list_by_tag"),
 ]
--- shopping/shopping_list/views.py
+++ shopping/shopping_list/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Item
+from .models import Item, Tag
 from . import forms
 from django.http import HttpResponseRedirect
 
@@ -9,10 +9,6 @@ def delete_item(request, item_id):
 	item.delete()
 	return redirect('shopping_list:list')
 
-def main_shopping_list(request):
-	items = Item.objects.all().order_by('date')
-	add = forms.AddItem()
-	return render(request, 'shopping_list/list.html', { 'items': items , 'add': add })
 
 def add_item(request):
 	if request.method == 'POST':
@@ -25,3 +21,23 @@ def add_item(request):
 	else:
 		add = forms.AddItem()
 	return render(request, 'shopping_list/list.html', { 'add': add })
+
+
+def main_shopping_list(request, tag_name=None):
+	active_tag = None
+	items = Item.objects.all().order_by('date')
+	tags = Tag.objects.all()
+	add = forms.AddItem()
+
+	if tag_name:
+		active_tag = Tag.objects.get(name=tag_name)
+		items = items.filter(tags__in=[active_tag])
+
+	context = {
+		'active_tag': active_tag,
+		'tags': tags,
+		'items': items,
+		'add': add,
+	}
+
+	return render(request, 'shopping_list/list.html', context)
