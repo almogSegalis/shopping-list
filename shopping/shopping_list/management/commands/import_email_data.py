8e0d9a0f1840d84e8f3518d5addd47be3f2d0584
almogSegalis <almogsegalis@gmail.com>
2023-03-05 16:17:02 +0200

    merged both email scripts to our django command

--- shopping/shopping_list/management/commands/import_email_data.py
+++ shopping/shopping_list/management/commands/import_email_data.py
@@ -8,9 +8,40 @@ import os
 import datetime
 import re
 
+import json
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from pprint import pprint
+import base64
+from email.mime.text import MIMEText
+
+CLASIFICATION_TIV_TAM = 'tivtaam'
+LOGO_TIV = 'https://d226b0iufwcjmj.cloudfront.net/retailers/1062/logo.png?0.11566895948753841'
+CLASIFICATION_JONGEL = 'חנויות מחמד'
+SHOPPING_TABLE_INDEX = 2
+RLEVANT_EMAIL_INDEX = 2
+
+# Set the scope for the Gmail API
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
 # --- add previous orders from emails ---
 SHOPPING_TABLE_INDEX = 2
-folder_path = "shopping_list/utils/emails"
+
+# Retrieve the content of a specific email message and write it to the file
+def get_message_body(message):
+    payload = message['payload']
+    parts = payload.get('parts', [])
+    data = None
+    for part in parts:
+        if part['mimeType'] == 'text/html':
+            data = part['body']['data']
+            break
+    if data is None:
+        data = payload['body'].get('data', '')
+    body = base64.urlsafe_b64decode(data).decode('utf-8')
+    return body
 
 def get_values_table_tiv(file):
     # Get the valus from the email table
@@ -43,6 +74,62 @@ class Command(BaseCommand):
     help = "Imports order data from gmail"
 
     def handle(self, *args, **options):
+        # Load the user's credentials from a file
+        creds_file = r'credentials.json'
+        creds = None
+        if os.path.exists('token.json'):
+            with open('token.json', 'r') as token_file:
+                creds_data = token_file.read()
+                creds = Credentials.from_authorized_user_info(
+                    info=json.loads(creds_data), scopes=SCOPES)
+
+        if not creds:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_file, SCOPES)
+            creds = flow.run_local_server(port=4000)
+
+            # Save the credentials to a file
+            with open('token.json', 'w') as token_file:
+                token_file.write(creds.to_json())
+
+        # Create a Gmail API client
+        service = build('gmail', 'v1', credentials=creds)
+
+        results = service.users().messages().list(userId='me').execute()
+        messages = results.get('messages', [])
+
+        for message in messages:
+            msg = service.users().messages().get(
+                    userId='me', id=message['id']).execute()
+            headers = msg['payload']['headers']
+            body = get_message_body(msg)
+            email_subject = next((header['value'] for header in headers if header['name'] == 'Subject'), None)
+            if (CLASIFICATION_TIV_TAM in body) or (LOGO_TIV in body):
+                update = False
+                if "עדכון" in email_subject:
+                    update = True
+                pattern = r"\d{2}/\d{2}/\d{4}\s\d{2}:\d{2}"
+                date_str = re.search(pattern, email_subject).group(0)
+                pattern = r"#\d+"
+                order_id = re.search(pattern, email_subject).group(0)
+                prefix = "#"
+                order_id = order_id.removeprefix(prefix)
+                date_str = date_str.replace('/','-').replace(' ', '_time_').replace(':', '-')
+                if update:    
+                    file_name = f'tiv_tam_update_date_{date_str}_id_{order_id}.html'
+                else:
+                    file_name = f'tiv_tam_date_{date_str}_id_{order_id}.html'
+            elif CLASIFICATION_JONGEL in email_subject:
+                file_name = f'JONGEL.html'
+            else:
+                file_name = f'None.html'
+            folder_path = "shopping_list/utils/emails/"
+            file_path = os.path.join(folder_path, file_name)
+            with open(file_path, 'w') as file:
+                file.write(f"Message ID: {message['id']}\n")
+                file.write(f"Body:\n{body}\n\n")
+
+
+        ###################################################################################################
         # inisialize the orders with data from emails
         for filename in os.listdir(folder_path):
             if os.path.isfile(os.path.join(folder_path, filename)):
